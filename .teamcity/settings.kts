958f3a7eba6f76d7823acabff4bb69df0809c368
Kierran McPherson <kierranm@gmail.com>
2019-08-29 19:44:28 +1200

    Release build

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,39 +3,23 @@ import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.PullRequests
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.pullRequests
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
 
 version = "2019.1"
 
 project {
-
+    params {
+        param("teamcity.ui.settings.readOnly", "true")
+        password("github.publishing_token", "credentialsJSON:e463dfac-587e-44e7-9aff-8f7faf91090f")
+    }
     buildType(Build)
+    buildType(PublishToRelease)
 }
 
 object Build : BuildType({
     name = "Build"
+    description = "Build and package the artifact"
 
     artifactRules = "+:artifactory-artifact-storage-server/build/distributions/artifactory-artifact-storage.zip"
 
@@ -77,6 +61,40 @@ object Build : BuildType({
                 authType = vcsRoot()
                 filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
             }
+        }
+    }
+})
+
+object PublishToRelease: BuildType({
+    name = "Publish"
+    description = "Publish Plugin to GitHub Release"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            name = "Rename Archive"
+            scriptContent = "mv artifactory-artifact-storage.zip artifactory-artifact-storage-%teamcity.build.branch%.zip"
+        }
+        script {
+            name = "Push Artifact to Release"
+            scriptContent = """
+                upload_url=$(curl -H "Authorization: token %github.publishing_token%" "https://api.github.com/repos/KierranM/teamcity-artifactory-artifact-storage-plugin/releases/tags/%teamcity.build.branch%" | jq .upload_url)
+                echo ${'$'}upload_url
+            """.trimIndent()
+        }
+    }
+
+    dependencies {
+        dependency(Build) {
+            artifacts {
+                artifactRules = "+:artifactory-artifact-storage.zip"
+                cleanDestination = true
+                buildRule = sameChainOrLastFinished()
+            }
+            snapshot {}
         }
     }
 })
