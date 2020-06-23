e0260096fcc5b2441efac8b33b77f6b81916b770
Kierran McPherson <kierranm@gmail.com>
2020-06-23 18:32:33 +1200

    Use chunked transfer encoding to avoid 404s

--- artifactory-artifact-storage-server/src/main/java/io/github/kierranm/teamcity/artifacts/artifactory/web/ArtifactoryArtifactDownloadProcessor.java
+++ artifactory-artifact-storage-server/src/main/java/io/github/kierranm/teamcity/artifacts/artifactory/web/ArtifactoryArtifactDownloadProcessor.java
@@ -7,19 +7,14 @@ import jetbrains.buildServer.artifacts.ArtifactData;
 import jetbrains.buildServer.serverSide.BuildPromotion;
 import jetbrains.buildServer.serverSide.artifacts.StoredBuildArtifactInfo;
 import jetbrains.buildServer.web.openapi.artifacts.ArtifactDownloadProcessor;
+import org.apache.commons.net.io.Util;
 import org.jetbrains.annotations.NotNull;
 import org.jfrog.artifactory.client.Artifactory;
 import org.jfrog.artifactory.client.RepositoryHandle;
-import org.jfrog.artifactory.client.model.File;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.channels.WritableByteChannel;
 import java.util.Map;
 import java.io.InputStream;
 
@@ -54,33 +49,16 @@ public class ArtifactoryArtifactDownloadProcessor implements ArtifactDownloadPro
       throw new IOException(message);
     }
 
-    RepositoryHandle repository = ArtifactoryUtil.getClient(params).repository(repositoryKey);
+    final Artifactory afClient = ArtifactoryUtil.getClient(params);
+
+    RepositoryHandle repository = afClient.repository(repositoryKey);
     final String artifactPath = artifactData.getPath();
     final String key = ArtifactoryUtil.getPathPrefix(storedBuildArtifactInfo.getCommonProperties()) + artifactPath;
-    File file = repository.file(key).info();
 
     InputStream fileStream = repository.download(key).doDownload();
 
-    httpServletResponse.addHeader("Content-Length", Long.toString(file.getSize()));
-    stream(fileStream, httpServletResponse.getOutputStream());
+    Util.copyStream(fileStream, httpServletResponse.getOutputStream());
+
     return true;
-  }
-
-  private long stream(InputStream input, ServletOutputStream output) throws IOException {
-    try (
-            ReadableByteChannel inputChannel = Channels.newChannel(input);
-            WritableByteChannel outputChannel = Channels.newChannel(output);
-    ) {
-      ByteBuffer buffer = ByteBuffer.allocateDirect(65536);
-      long size = 0;
-
-      while (inputChannel.read(buffer) != -1) {
-        buffer.flip();
-        size += outputChannel.write(buffer);
-        buffer.clear();
-      }
-
-      return size;
-    }
   }
 }
